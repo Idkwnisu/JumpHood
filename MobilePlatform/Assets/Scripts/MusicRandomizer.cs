c49f0ffe11af4fe95d935932c2c337c3dcb5ec89
Stefano Palma <stefano.palma1@studenti.unimi.it>
2020-09-06 15:48:05 +0200

    music + fix

---
+++ MobilePlatform/Assets/Scripts/MusicRandomizer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicRandomizer : MonoBehaviour
+{
+    public AudioClip[] clips;
+    private AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        RandomizeMusic();
+    }
+
+    public void RandomizeMusic()
+    {
+        int rnd = Random.Range(0, clips.Length);
+        source.clip = clips[rnd];
+        source.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
